3bc7583b47f535b45a69d8eae1e4da35372c6e05
Richard Barnes <rlb@ipv.sx>
2014-12-20 18:23:39 -0500

    Removed draft node implementations, since it's in node-acme/restify

--- node/test.js
+++
@@ -1,9 +0,0 @@
-var crypto = require("./crypto-util");
-
-var keyPair = crypto.generateKeyPair(512);
-
-var payload = new Buffer("hello, world!");
-var jws = crypto.generateSignature(keyPair, payload);
-console.log(JSON.stringify(jws));
-
-console.log(crypto.verifySignature(jws));
